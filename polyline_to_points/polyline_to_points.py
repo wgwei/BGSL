c9dbc2be88016562507a864c8f70907fddd480d4
wgwei <wgang.wei@gmail.com>
2014-01-07 19:23:19 +0100

    remove the useles shape files

--- polyline_to_points/polyline_to_points.py
+++ polyline_to_points/polyline_to_points.py
@@ -6,8 +6,6 @@ split polyline to points by distance between adjacent points
 
 """
 
-import sys
-sys.path.append(r'/home/wgwei/Documents/projects/SSM/test')
 import shapelib
 import dbflib
 import numpy as np
@@ -24,7 +22,7 @@ class PolylineShape():
     def write_point_shape_out(self, pointShapeFileName, pointsList):
         w2shp = shapelib.create(pointShapeFileName, shapelib.SHPT_POINT)
         w2dbf = dbflib.create(pointShapeFileName)
-        w2dbf.add_field('ID', dbflib.FTInteger, 10, 0)
+        w2dbf.add_field('ID', dbflib.FTInteger, 10, 0) # create 3 field for the ID and x, y coordinate
         w2dbf.add_field('x', dbflib.FTDouble, 16, 2)
         w2dbf.add_field('y', dbflib.FTDouble, 16, 2)
         i = 0
@@ -43,14 +41,20 @@ class PolylineShape():
         allPoints = []
         for m in xrange(self.PLNUM):
             print 'processing polyline %d' %m
+            # read shape object and feed function polyline_to_points_by_eqDist
             pobj = self.shp.read_object(m)
             polyline = pobj.vertices()[0]
             points = self.polyline_to_points_by_eqDist(polyline, distance)
             allPoints.append(points)
         self.shp.close()
-        return allPoints
+        return allPoints # a list of points for different polyline segments as: [[(x1, y1), (x2, y2)...], [(x1, y1), (x2, y2)...]]
         
     def polyline_to_points_by_eqDist(self, polyline, distance):
+        ''' Split the polyline to points by distance. 
+            polyline = [(x1, y1), (x2, y2), (x3, y3)...] is a list of points
+            distance = number. The distance between two adjacent points. The 
+            distance is along the polyline NOT the direct distance from two points
+        '''
         i = 0
         dni = 0.0  #distance from the inserted pt to the vertice of polyline
         points = []   # coordinate of the splitted points as (x,y)
@@ -66,21 +70,21 @@ class PolylineShape():
                 ix = ix+(distance-(dni-distij))*(polyline[i+1][0]-polyline[i][0])/distij
                 iy = iy+(distance-(dni-distij))*(polyline[i+1][1]-polyline[i][1])/distij
                 points.append((ix, iy))
-                ixiy2next = dist2D((ix, iy), polyline[i+1])
+                ixiy2next = dist2D((ix, iy), polyline[i+1]) # updating the distance from the inserting points to the next vertice of the polyline
                 while ixiy2next>distance:
                     ix = ix+distance*(polyline[i+1][0]-polyline[i][0])/distij
                     iy = iy+distance*(polyline[i+1][1]-polyline[i][1])/distij
                     points.append((ix, iy))
                     ixiy2next = dist2D((ix,iy), polyline[i+1])
-                dni = ixiy2next
+                dni = ixiy2next # updating again
             i = i+1
-        return points               
+        return points    # point list as: [(x1, y1), (x2, y2), (x3, y3)...]       
            
-def main():
-    pobj = PolylineShape(r'tram')
-    pointlist = pobj.polyShape_to_points_by_eqDist(10.0)
-    pobj.write_point_shape_out('ti', pointlist)
+def split_polyline_to_points(shapeFileName, distance, newPointShapeFileName):
+    pobj = PolylineShape(shapeFileName)
+    pointlist = pobj.polyShape_to_points_by_eqDist(distance)
+    pobj.write_point_shape_out(newPointShapeFileName, pointlist)
                 
     
 if __name__=='__main__':
-    main()
+    split_polyline_to_points()
